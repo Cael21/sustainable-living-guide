974c95d14ec854573dc20e2c3be00d9e0e093f1e
Cael Amerman <caelamerman@gmail.com>
2024-05-16 23:22:58 -0400

    Test component with Material UI

---
+++ src/components/MyComponent.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Button } from '@mui/material';
+
+const MyComponent = () => {
+  return (
+    <div>
+      <Button variant="contained" color="primary">
+        Hello Material-UI
+      </Button>
+    </div>
+  );
+};
+
+export default MyComponent;
